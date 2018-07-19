0b6b3213e59bff70e1c1c6aba6f90f5ed5a9708e
Tina Lee <tinalee@2001-b400-e257-c248-2523-7aaf-24e3-e8b5.emome-ip6.hinet.net>
2018-07-19 16:53:33 +0800

    app

--- app1/SecondViewController.swift
+++ app1/SecondViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+   
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBAction func importimage(_ sender: Any) {
+        
+            let image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            image.allowsEditing = false
+            self.present(image,animated: true)
+            {
+                //After it is complete
+            }
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            if let image = info[UIImagePickerControllerOriginalImage]as?UIImage{
+                
+                ImageView.image = image
+            }
+            else{
+                ImageView.image = nil
+                //Error message
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
